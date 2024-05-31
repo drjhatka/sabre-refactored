728971ccc0757ee89301f0a5d054aa209bd4c06d
drjhatka <bishawjit12@gmail.com>
2024-05-30 22:04:05 -0400

    daisyui installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [ 
-            "./index.html",
-          "./src/**/*.{js,ts,jsx,tsx}",
+           require('daisyui'),
         ],
 }
 
